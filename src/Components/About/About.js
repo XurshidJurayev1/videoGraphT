47589097a155f7db0efec2b8a09b2e87fb717e7d
Xurshid-web <73391206+Xurshid-web@users.noreply.github.com>
2021-04-10 10:11:13 +0500

    10

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Home4 from '../home/Home4'
 import Home5 from '../home/Home5'
 import About2 from './About2'
-import About3 from './About3'
+// import About3 from './About3'
 
 const About = () => {
     return (
         <div>
             <About2/>
-            <About3/>
+            {/* <About3/> */}
             <Home4/>
             <Home5/>
         </div>
